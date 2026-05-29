accbdb1e610b9596f1136f8761de48fc9914df46
rohit2005kumar <rohitkumarroy234987@gmail.com>
2026-05-29 18:55:10 +0530

    conflites has been resolved

--- Frontend/src/main.jsx
+++ Frontend/src/main.jsx
@@ -3,7 +3,7 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
 import { BrowserRouter } from 'react-router-dom'
-<<<<<<< HEAD
+
 import { AuthProvider } from './context/authcontext.jsx'
 import { PortfoiloContextProvider } from './context/portfoiloContext.jsx'
 
@@ -13,18 +13,10 @@ createRoot(document.getElementById('root')).render(
     <BrowserRouter>
     <PortfoiloContextProvider>
     <AuthProvider>
-      
       <App />
       </AuthProvider>
       </PortfoiloContextProvider>
-=======
-
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <BrowserRouter>
-      <App />
->>>>>>> c35557581e1e15227bbe74e58fa8df71f267adc6
-    </BrowserRouter>
+      </BrowserRouter>
   
   </StrictMode>,
 )
